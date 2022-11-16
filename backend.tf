ce09d5eb38a806910b1296dc5dd97b5323b3b441
Mahesh Ramachandrappa <maramachandr@svb.com>
2022-11-16 10:54:12 +0530

    Added backend configuration

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-   bucket         = "terraform_backend"
+   bucket         = "relx-terraform-backend"
    key            = "state/terraform.tfstate"
    region         = "us-east-1"
    dynamodb_table = "terraform-state"
